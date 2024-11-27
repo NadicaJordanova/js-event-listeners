c14f3aaf3f297e8ec3fe410ffcff2be22edafdbb
NadicaJordanova <nadicajordanova3@gmail.com>
2024-11-27 17:36:33 +0100

    commit

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var emailInput = document.getElementById("email");
 var indexNumbInput = document.getElementById("indexnumb");
 var dateInput = document.getElementById("dateofbirth");
 
-
 // enterBtn.addEventListener("click", () =>{
 // console.log(nameInput.value)
 // console.log(emailInput.value)
@@ -16,7 +15,6 @@ var dateInput = document.getElementById("dateofbirth");
 // document.getElementById('dateofbirth').value='';
 // })
 
-
 // var student1 = {
 //     studenName: nameInput.value,
 //     studentEmail: emailInput.value,
@@ -25,27 +23,43 @@ var dateInput = document.getElementById("dateofbirth");
 // }
 // console.log(student1)
 
-
-students =[]
-enterBtn.addEventListener("click", () =>{
-
-    // console.log(nameInput.value)
-    // console.log(emailInput.value)
-    // console.log(indexNumbInput.value)
-    // console.log(dateInput.value)
-    var student = {
+var studentsContainer = document.getElementById("studentcard");
+students = [];
+enterBtn.addEventListener("click", () => {
+  // console.log(nameInput.value)
+  // console.log(emailInput.value)
+  // console.log(indexNumbInput.value)
+  // console.log(dateInput.value)
+  var student = {
     studentName: nameInput.value,
     studentEmail: emailInput.value,
     studentDateOfBirth: dateInput.value,
     studentIndexNumber: indexNumbInput.value,
-}
-console.log(student)
-students.push(student)
-console.log(students)
+  };
+  console.log(student);
+  students.push(student);
+  console.log(students);
 
-    document.getElementById("studentcard").innerHTML = '';
-    students.forEach(student => {
-     document.getElementById("studentcard").innerHTML +=`
+  document.getElementById("studentcard").innerHTML = "";
+  students.forEach((student) => {
+    // const divElement = document.createElement("div");
+    // divElement.classList.add("stdcard");
+    // const h2Element = document.createElement("h2");
+    // h2Element.innerText = `E-index`;
+    // const h31Element = document.createElement("h3");
+    // h31Element.innerText = `Student name: ${student.studentName}`;
+    // const h32Element = document.createElement("h3");
+    // h32Element.innerText = `Student email: ${student.studentEmail}`;
+    // const h33Element = document.createElement("h3");
+    // h33Element.innerText = `Index number: ${student.studentIndexNumber}`;
+
+    // studentsContainer.appendChild(divElement);
+    // divElement.appendChild(h2Element);
+    // divElement.appendChild(h31Element);
+    // divElement.appendChild(h32Element);
+    // divElement.appendChild(h33Element);
+
+    document.getElementById("studentcard").innerHTML += `
        <div class='stdcard'>
        <h2>E-index</h2>
        <h3>Student name: ${student.studentName}</h3>
@@ -54,20 +68,10 @@ console.log(students)
        <h3>Index number: ${student.studentIndexNumber}</h3>
        </div>
     
-       `
-        
-    });
-    document.getElementById('nameandsurname').value='';
-    document.getElementById('email').value='';
-    document.getElementById('indexnumb').value='';
-    document.getElementById('dateofbirth').value='';
-    })
-   
-  
- 
-
-
-
-
-
-
+       `;
+  });
+  document.getElementById("nameandsurname").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("indexnumb").value = "";
+  document.getElementById("dateofbirth").value = "";
+});
